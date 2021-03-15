427b562159e0ba91a04a1cb01bcae36521ca8f4b
MaymarHany <34577145+MaymarHany@users.noreply.github.com>
2021-03-15 11:58:50 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    
+    
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login'
+import Userlist from './components/UserList'
+import Profile from './components/Profile.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Login,
+    Userlist,
+    Profile
+  },
+  data(){
+    return{
+    
+        
+        }
+  },
+  props:{
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
